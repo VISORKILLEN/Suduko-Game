a47f2032c4fb69a99faa58b9dbb2c83f19af1725
VISORKILLEN <viktor.gustavsson@live.se>
2025-12-05 07:41:11 +0100

    Made new class to make programs.cs more clean

---
+++ Suduko Game/PlayGame.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Suduko_Game
+{
+    internal class PlayGame
+    {
+
+        internal static void Run()
+        {
+            StartGame();
+
+        }
+
+        internal static void StartGame()
+        {
+            Console.Clear();
+
+            int[,] board = SudukoGenerator.GenerateSuduko(40);
+            bool gameRunning = true;
+            while (gameRunning && !SudukoGenerator.isSolved(board))
+            {
+                Console.Clear();
+                PrintBoard(board);
+                Console.Write("\nRad 1-9, 0 för av avsluta: ");
+
+                if (!int.TryParse(Console.ReadLine(), out int r) || r < 0 || r > 9)
+                {
+                    continue;
+                }
+
+                if (r == 0)
+                {
+                    gameRunning = false;
+                    break;
+                }
+
+                Console.Write("Kolumn (1-9): ");
+                if (!int.TryParse(Console.ReadLine(), out int c) || c < 1 || c > 9)
+                {
+                    continue;
+                }
+
+                Console.Write("Siffra (1-9): ");
+                if (!int.TryParse(Console.ReadLine(), out int num) || num < 1 || num > 9)
+                {
+                    continue;
+                }
+
+                if (SudukoGenerator.IsSafe(board, r - 1, c - 1, num))
+                {
+                    board[r - 1, c - 1] = num;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Siffra placerad");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ogiltlig placering av nummer");
+                    Console.ResetColor();
+                }
+
+                Console.WriteLine("Tryck på valfri tanget för att fortsätta.");
+                Console.ReadKey();
+            }
+
+            if (SudukoGenerator.isSolved(board))
+            {
+                Console.Clear();
+                PrintBoard(board);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Grattis! Du har löst Sudokut!");
+                Console.ResetColor();
+                Console.Write("Vill du spela igen? y/n: ");
+
+                string answer = Console.ReadLine().ToLower();
+
+                if (answer == "y")
+                {
+                    StartGame();
+                }
+                else if (answer == "n")
+                {
+                    Console.WriteLine("Tack för att du har spelat mitt suduko:");
+                }
+            }
+        }
+
+        static void PrintBoard(int[,] board)
+        {
+
+            for (int r = 0; r < 9; r++)
+            {
+                if (r % 3 == 0 && r != 0)
+                {
+                    Console.WriteLine("------+-------+------");
+                }
+
+                for (int c = 0; c < 9; c++)
+                {
+                    if (c % 3 == 0 && c != 0)
+                    {
+                        Console.Write("| ");
+                    }
+                    int val = board[r, c];
+                    Console.Write(val == 0 ? ". " : val + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
