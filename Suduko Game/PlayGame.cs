ed0df738caa343974162607e844315f9da1c1019
VISORKILLEN <viktor.gustavsson@live.se>
2025-12-05 08:10:16 +0100

    Fixed a new method to restartgame and solvedBoard method

--- Suduko Game/PlayGame.cs
+++ Suduko Game/PlayGame.cs
@@ -10,92 +10,108 @@ namespace Suduko_Game
     internal class PlayGame
     {
 
+        static int[,] board = SudukoGenerator.GenerateSuduko(40);
         internal static void Run()
         {
-            StartGame();
+            PlaySuduko();
         }
 
-        internal static void StartGame()
+        internal static void PlaySuduko()
         {
-            Console.Clear();
+            bool playing = true;
 
-            
-            int[,] board = SudukoGenerator.GenerateSuduko(40);
-            bool gameRunning = true;
-            while (gameRunning && !SudukoGenerator.isSolved(board))
+            while (playing)
             {
                 Console.Clear();
-                PrintBoard(board);
-                Console.Write("\nRad 1-9, 0 för av avsluta: ");
 
-                if (!int.TryParse(Console.ReadLine(), out int r) || r < 0 || r > 9)
+                bool gameRunning = true;
+                while (gameRunning && !SudukoGenerator.isSolved(board))
                 {
-                    continue;
+                    Console.Clear();
+                    PrintBoard();
+                    Console.Write("\nRad 1-9, 0 för av avsluta: ");
+
+                    if (!int.TryParse(Console.ReadLine(), out int r) || r < 0 || r > 9)
+                    {
+                        continue;
+                    }
+
+                    if (r == 0)
+                    {
+                        gameRunning = false;
+                        break;
+                    }
+
+                    Console.Write("Kolumn (1-9): ");
+                    if (!int.TryParse(Console.ReadLine(), out int c) || c < 1 || c > 9)
+                    {
+                        continue;
+                    }
+
+                    Console.Write("Siffra (1-9): ");
+                    if (!int.TryParse(Console.ReadLine(), out int num) || num < 1 || num > 9)
+                    {
+                        continue;
+                    }
+
+                    if (SudukoGenerator.IsSafe(board, r - 1, c - 1, num))
+                    {
+                        board[r - 1, c - 1] = num;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Siffra placerad");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ogiltlig placering av nummer");
+                        Console.ResetColor();
+                    }
+
+                    Console.WriteLine("Tryck på valfri tanget för att fortsätta.");
+                    Console.ReadKey();
                 }
 
-                if (r == 0)
-                {
-                    gameRunning = false;
-                    break;
-                }
+                SolvedBoard();
+                RestartGame();
+            }
+        }
 
-                Console.Write("Kolumn (1-9): ");
-                if (!int.TryParse(Console.ReadLine(), out int c) || c < 1 || c > 9)
-                {
-                    continue;
-                }
+        static void RestartGame(bool playing = true)
+        {
+            Console.Write("Vill du spela igen? y/n: ");
 
-                Console.Write("Siffra (1-9): ");
-                if (!int.TryParse(Console.ReadLine(), out int num) || num < 1 || num > 9)
-                {
-                    continue;
-                }
+            string answer = Console.ReadLine().ToLower();
 
-                if (SudukoGenerator.IsSafe(board, r - 1, c - 1, num))
-                {
-                    board[r - 1, c - 1] = num;
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Siffra placerad");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Ogiltlig placering av nummer");
-                    Console.ResetColor();
-                }
-
-                Console.WriteLine("Tryck på valfri tanget för att fortsätta.");
-                Console.ReadKey();
+            if (answer == "y")
+            {
+                Console.Clear();
+            }
+            else if (answer == "n")
+            {
+                Console.WriteLine("Tack för att du har spelat mitt suduko!");
+                playing = false;
+            }
+            else
+            {
+                Console.WriteLine("Felaktigt val, skriv igen.");
             }
 
+        }
+
+        private static void SolvedBoard()
+        {
             if (SudukoGenerator.isSolved(board))
             {
                 Console.Clear();
-                PrintBoard(board);
+                PrintBoard();
                 Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine("Grattis! Du har löst Sudokut!");
                 Console.ResetColor();
-                Console.Write("Vill du spela igen? y/n: ");
-
-                string answer = Console.ReadLine().ToLower();
-
-                if (answer == "y")
-                {
-                    StartGame();
-                }
-                else if (answer == "n")
-                {
-                    Console.WriteLine("Tack för att du har spelat mitt suduko!");
-                }
-                else
-                {
-                    Console.WriteLine("Felaktigt val, skriv igen.");
-                }
             }
         }
 
-        static void PrintBoard(int[,] board)
+        static void PrintBoard()
         {
 
             for (int r = 0; r < 9; r++)
