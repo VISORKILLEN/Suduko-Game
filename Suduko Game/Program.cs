c6e8afae7479a35c99366c376882f752d42f4121
VISORKILLEN <viktor.gustavsson@live.se>
2025-12-22 17:57:30 +0100

    Added a switch case, going to add other diffeculties

--- Suduko Game/Program.cs
+++ Suduko Game/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            PlayGame.Run();
+
+            bool playing = true;
+
+            while (playing)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Välkommen till spelkonsolen\n" +
+                    "Vilken svårihetsgrad vill du köra?\n" +
+                    "1. Enkel\n" +
+                    "2. Vanlig\n" +
+                    "3. Svår\n" +
+                    "0. Stäng av spelkonsolen");
+
+                int choice;
+                int.TryParse(Console.ReadLine(), out choice);
+
+                switch (choice)
+                {
+                    case 1:
+                        break;
+
+                    case 2:
+                        PlayGame.Run();
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 0:
+                        playing = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Felaktig inmating\n" +
+                            "Tryck på valfri knapp för att gå tillbaka till menyn.");
+                        Console.ReadLine();
+                        break;
+
+                }
+
+            }
+
         }
     }
 }
